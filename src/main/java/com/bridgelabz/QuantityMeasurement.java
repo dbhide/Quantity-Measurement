c612e0cf9727374af78344fdfccbffac672f1b47
dbhide <dhanashree.bhide3@gmail.com>
2020-03-14 11:49:59 +0530

    ADDED: Function to compare two values

--- src/main/java/com/bridgelabz/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/QuantityMeasurement.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
 public class QuantityMeasurement {
-
+    public boolean compareLength(int value1, int value2) {
+        return value1 == value2;
+    }
 }
--- src/test/java/com/bridgelabz/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest.java
@@ -1,12 +1,20 @@
 package com.bridgelabz;
 
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class QuantityMeasurementTest {
     QuantityMeasurement quantityMeasurement;
     @Before
     public void setUp() throws Exception {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        quantityMeasurement = new QuantityMeasurement();
+    }
+
+    @Test
+    public void given0Feet_WhenComparedWith0Feet_ShouldReturnEqual() {
+        boolean result = quantityMeasurement.compareLength(0, 0);
+        Assert.assertTrue(result);
     }
 }
