d0e94d1f92d75828cef7733c7caeb00aecdb1156
dbhide <dhanashree.bhide3@gmail.com>
2020-03-14 11:38:04 +0530

    ADDED: Created object of the main class

---
+++ src/test/java/com/bridgelabz/QuantityMeasurementTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+
+import org.junit.Before;
+
+public class QuantityMeasurementTest {
+    QuantityMeasurement quantityMeasurement;
+    @Before
+    public void setUp() throws Exception {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+    }
+}
